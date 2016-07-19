f4423ff5ecca854c5c531706f8859ed703f07dc0
mieshajihan <mieshajw@gmail.com>
2016-07-18 21:32:59 -0500

    updated routes and views for customer and home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711213949) do
+ActiveRecord::Schema.define(version: 20160719001557) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -48,6 +48,11 @@ ActiveRecord::Schema.define(version: 20160711213949) do
 
   add_index "providers", ["email"], name: "index_providers_on_email", unique: true
   add_index "providers", ["reset_password_token"], name: "index_providers_on_reset_password_token", unique: true
+
+  create_table "requests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.string   "origin1"
---
+++ test/controllers/request_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RequestControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
